6c9069ab350d1c1af82904e5dd9f00de5aefe919
Ajmain Nishu <ajmainnishu@gmail.com>
2021-09-05 15:17:35 +0600

    feat: Module_38 added

---
+++ milestone-7/module_38/module_38-2/index.js
@@ -0,0 +1,49 @@
+// const aleartModal = () => {
+//     alert('Hello')
+// }
+
+
+
+
+
+// const aleartModal = () => {
+//     confirm('Hello')
+// }
+
+
+
+
+
+// const aleartModal = () => {
+//     const response = confirm('Hello')
+//     console.log(response)
+// }
+
+
+
+
+
+// const aleartModal = () => {
+//     const response = confirm('are you going to picnic')
+//     if (response === true) {
+//         alert('amake fee ta bkash kore de')
+//     } else {
+//         console.log('Dure giya mor!!!')
+//     }
+// }
+
+
+
+
+
+// const askName = () => {
+//     prompt('What is your name ?')
+// }
+
+
+
+
+
+// const askName = () => {
+//     console.log(prompt('What is your name ?'))
+// }
---
+++ milestone-7/module_38/module_38-3/index.js
@@ -0,0 +1,27 @@
+// const pageLocation = () => {
+//     location.reload()
+// }
+
+
+
+
+
+// const pageLocation = () => {
+//     location.assign('https://www.google.com/')
+// }
+
+
+
+
+
+// const pageLocation = () => {
+//     location.href = 'https://www.google.com/'
+// }
+
+
+
+
+
+// const pageLocation = () => {
+//     location.hash = '#render'
+// }

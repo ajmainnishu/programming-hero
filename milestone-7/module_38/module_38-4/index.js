bad0e75895cf3046cb2e7c8e7823797429fbe58c
Ajmain Nishu <ajmainnishu@gmail.com>
2021-09-09 16:31:56 +0600

    feat: Added lecture 38_4 in module_38 in milestone-7

---
+++ milestone-7/module_38/module_38-4/index.js
@@ -0,0 +1,35 @@
+// const historyPage = () => {
+//     console.log(history)
+// }
+
+
+
+
+
+// const historyPage = () => {
+//     console.log(history.length)
+// }
+
+
+
+
+
+// const historyPage = () => {
+//     console.log(history.back())
+// }
+
+
+
+
+
+// const historyPage = () => {
+//     console.log(history.forward())
+// }
+
+
+
+
+
+// const historyPage = () => {
+//     console.log(history.go())
+// }

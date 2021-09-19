653f2d4cbe5664cc0e7985232f93080162e90bf3
Ajmain Nishu <ajmainnishu@gmail.com>
2021-09-19 09:54:11 +0600

    feat: Added module_41 in milestone-7

---
+++ milestone-7/module_41/module_41-7/js/index.js
@@ -0,0 +1,42 @@
+"use strict";
+// interface Player {
+//     name: string,
+//     club: string,
+//     salary: number,
+//     wife?: string,
+//     previousClubs?: string[]
+// }
+// const messy: Player = {
+//     name: 'L Messy',
+//     club: 'Abahoni',
+//     salary: 12000,
+//     wife: 'Nosimon Ara Begum'
+// }
+// const hannan: Player = {
+//     club: 'Kolabagan',
+//     salary: 2000,
+//     name: 'A.  Hannan',
+// }
+// console.log(hannan);
+// console.log(messy);
+// interface Employee {
+//     name: string,
+//     designation: string,
+//     salary: number,
+//     age: number
+// }
+// interface Developer extends Employee {
+//     language: string,
+//     codeEditor: string,
+//     typingSpeed: number
+// }
+// const zuku: Developer = {
+//     name: 'Zuku Zuku',
+//     designation: 'Web Developer',
+//     language: 'JS',
+//     codeEditor: 'VS Code',
+//     typingSpeed: 65,
+//     age: 31,
+//     salary: 30000
+// }
+// console.log(zuku)
---
+++ milestone-7/module_41/module_41-8/index.ts
@@ -0,0 +1,38 @@
+// class Car {
+//     modal: string;
+//     price: number;
+//     constructor(modal: string, price: number) {
+//         this.modal = modal;
+//         this.price = price
+//     }
+//     getTotalPrice(tax: number): number{
+//         const taxAmount = this.price * tax / 100;
+//         const total = this.price + taxAmount;
+//         return total;
+//     }
+// }
+// const toyota = new Car('toyota', 100000)
+// console.log(toyota);
+// const priceWithTax: number = toyota.getTotalPrice(25);
+// console.log(priceWithTax);
+
+
+
+
+
+// class Car {
+//     modal: string;
+//     price: number;
+//     private _millage: number;
+//     constructor(modal: string, price: number) {
+//         this.modal = modal;
+//         this.price = price
+//         this._millage = 100000;
+//     }
+//     getActualMillage(): number {
+//         const realMillage = this._millage + 50000;
+//         return realMillage;
+//     }
+// }
+// const toyota = new Car('toyota', 100000)
+// console.log(toyota);

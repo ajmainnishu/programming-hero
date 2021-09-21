8ed08a404ba6ce9f1be0d342a9fe055831784633
Ajmain Nishu <ajmainnishu@gmail.com>
2021-09-21 15:50:39 +0600

    feat: Added module_47 in milestone-8

---
+++ milestone-8/module_47/module_47-4/src/App.js
@@ -0,0 +1,109 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+
+
+
+
+// function App() {
+//   const [steps, setSteps] = useState(0);
+//   const handleIncreaseSteps = () => {
+//     const newSteps = steps + 1;
+//     setSteps(newSteps)
+//     console.log(steps)
+//   }
+//   return (
+//     <div>
+//       <h3>Steps: {steps}</h3>
+//       <button onClick={handleIncreaseSteps}>Walk</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+// function App() {
+//   const [steps, setSteps] = useState(0);
+//   const handleIncreaseSteps = () => {
+//     const newSteps = steps + 1;
+//     setSteps(newSteps)
+//     console.log(newSteps)
+//   }
+//   return (
+//     <div>
+//       <h3>Steps: {steps}</h3>
+//       <button onClick={handleIncreaseSteps}>Walk</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+// function App() {
+//   const [steps, setSteps] = useState(0);
+//   const handleIncreaseSteps = () => {
+//     const newSteps = steps + 1;
+//     setSteps(newSteps)
+//   }
+//   useEffect(() => {
+//     console.log(steps)
+//   }, [])
+//   return (
+//     <div>
+//       <h3>Steps: {steps}</h3>
+//       <button onClick={handleIncreaseSteps}>Walk</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+// function App() {
+//   const [steps, setSteps] = useState(0);
+//   const handleIncreaseSteps = () => {
+//     const newSteps = steps + 1;
+//     setSteps(newSteps)
+//   }
+//   useEffect(() => {
+//     console.log(steps)
+//   })
+//   return (
+//     <div>
+//       <h3>Steps: {steps}</h3>
+//       <button onClick={handleIncreaseSteps}>Walk</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+// function App() {
+//   const [steps, setSteps] = useState(0);
+//   const handleIncreaseSteps = () => {
+//     const newSteps = steps + 1;
+//     setSteps(newSteps)
+//   }
+//   useEffect(() => {
+//     console.log(steps)
+//   }, [steps])
+//   return (
+//     <div>
+//       <h3>Steps: {steps}</h3>
+//       <button onClick={handleIncreaseSteps}>Walk</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+export default App;

7cb9573c495867eaaf4a70f03f96c263d3e4ec7c
Ajmain Nishu <ajmainnishu@gmail.com>
2021-10-13 19:44:30 +0600

    feat: Added module_61 in milestone-10

---
+++ milestone-10/module_61/module_61-1/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import Home from './Pages/Home/Home/Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router dom'
+import NotFound from './Pages/NotFound/NotFound';
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
---
+++ milestone-10/module_61/module_61-6/src/Pages/Login/Login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+
+const Login = () => {
+    return (
+        <div>
+            <h2>Please Login</h2>
+            <button className="btn-warning">Google Sign In</button>
+        </div>
+    );
+};
+
+
+
+
+
+export default Login;

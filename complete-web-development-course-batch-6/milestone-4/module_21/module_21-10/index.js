344e7dc613b84f6a81658ab50a0127c8d9c00365
Ajmain Nishu <ajmainnishu@gmail.com>
2023-06-21 20:26:13 +0600

    Remove module_21-10

--- complete-web-development-course-batch-6/milestone-4/module_21/module_21-10/index.js
+++
@@ -1,9 +0,0 @@
-// Module Summary and where you need to focus
-
-
-
-// 1. Write a code. With which one can extract the smallest number in an array.
-// 2. Write a code that will find the smallest number out of three numbers.
-// 3. Write a function. The function will take an array as input. That array will contain many numbers. Your task will be to input as many numbers as there are in the array. Find the average of those numbers. Then return that average function. think a little try to understand try it see if you can
-// 4. Write a function. which will take the length and height of a rectangle as input parameters. Then return the area of ​​that rectangle as the result.
-// 5. An array contains many numbers. Write a program to find the second largest number from those numbers. Search Google if necessary. Then look at the search results and try to understand.

ca73787be6c3a10f9d32a323681d465a3536bebf
Ajmain Nishu <ajmainnishu@gmail.com>
2023-07-02 12:42:08 +0600

    feat: Module_25 added in milestone-5

---
+++ complete-web-development-course-batch-6/milestone-5/module_25/module_25-2/index.js
@@ -0,0 +1,11 @@
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+} 

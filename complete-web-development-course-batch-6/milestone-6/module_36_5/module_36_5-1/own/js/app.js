2ba4fd5e26123367819cc2f1f4c152926bec1984
Ajmain Nishu <ajmainnishu@gmail.com>
2023-07-27 22:51:34 +0600

    feat: Module_36_5 added in milestone-6

---
+++ complete-web-development-course-batch-6/milestone-6/module_36_5/module_36_5-1/own/js/app.js
@@ -0,0 +1,12 @@
+const API_KEY = ``;
+const loadTemperature = city => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+}
+const displayTemperature = data => {
+    const temperature = document.getElementById('temperature');
+    temperature.innerText = data.main.temp;
+}
+loadTemperature('dhaka');

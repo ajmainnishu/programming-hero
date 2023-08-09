710b0fbd5ef1b9f4397b12c69d7d1b186c3d99d0
Ajmain Nishu <ajmainnishu@gmail.com>
2023-08-09 21:44:13 +0600

    feat: Module_43 added in milestone-7

---
+++ complete-web-development-course-batch-7/milestone-7/module_43/module_43-3/index.js
@@ -0,0 +1,69 @@
+// Console tab, preserve log, log levels, console api
+
+
+
+// console.log();
+
+
+
+
+// console.assert();
+
+
+
+
+// console.clear();
+
+
+
+
+// console.count();
+
+
+
+
+// console.error('hello');
+
+
+
+
+// console.time()
+
+
+
+
+// const products = [
+//     {id: 1, name: 'mobile', price: 5000, quantity: 5},
+//     {id: 2, name: 'tablet1', price: 55000, quantity: 15}
+// ];
+// console.table(products);
+
+
+
+
+// console.warn('hello');
+
+
+
+
+// monitor(function name)
+
+
+
+
+// unmonitor(function name)
+
+
+
+
+// monitorEvents(element name)
+
+
+
+
+// unmonitorEvents(element name)
+
+
+
+
+// monitorEvents(element name ['click', 'mouseout'])

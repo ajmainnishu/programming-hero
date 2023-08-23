ad34b6d5e8f007ca057bdbdc75f8dd54c05fe94b
Ajmain Nishu <ajmainnishu@gmail.com>
2023-08-23 09:40:13 +0600

    feat: Module_49 added in milestone-8

---
+++ complete-web-development-course-batch-7/milestone-8/module_49/module_49-3/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Device from './components/Device/Device'
+
+function App() {
+  return (
+    <>
+      <Device name="uphone" price="1300"></Device>
+      <Device name="sumsing" price="1100"></Device>
+    </>
+  )
+}
+
+export default App
---
+++ complete-web-development-course-batch-7/milestone-8/module_49/module_49-4/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Knob from './components/Knob/Knob'
+import Watch from './components/Watch/Watch'
+
+function App() {
+  return (
+    <>
+      <Knob steps="456"></Knob>
+      <Watch></Watch>
+    </>
+  )
+}
+
+export default App

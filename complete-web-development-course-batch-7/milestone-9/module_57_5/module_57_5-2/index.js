cb8b6f8d396272a8b028cb794e86fd6b2cc286a1
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-18 18:00:39 +0600

    feat: Module_57_5 added in milestone-9

---
+++ complete-web-development-course-batch-7/milestone-9/module_57_5/module_57_5-2/index.js
@@ -0,0 +1,23 @@
+// Create get all data api and single phone api
+
+
+
+const express = require('express');
+const phones = require('./phones.json');
+const app = express();
+const port = 5000;
+app.get('/', (req, res) => {
+    res.send('my phone server coming soon');
+})
+app.get('/phones', (req, res) => {
+    res.send(phones);
+})
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('i need data for id: ', id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone)
+})
+app.listen(port , () => {
+    console.log(`My phone server is running on port: ${port}`)
+})

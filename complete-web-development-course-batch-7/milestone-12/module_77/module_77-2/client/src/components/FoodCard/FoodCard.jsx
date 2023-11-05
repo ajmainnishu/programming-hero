49342ace6f7d402c79cdafcf6bf3ad63d16912c1
Ajmain Nishu <ajmainnishu@gmail.com>
2023-11-06 00:25:19 +0600

    feat: Module_77 added in milestone-12

---
+++ complete-web-development-course-batch-7/milestone-12/module_77/module_77-2/client/src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,55 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+const FoodCard = ({ item }) => {
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { name, image, recipe, price } = item;
+    const handleAddToCart = item => {
+        if (user) {
+            fetch(`http://localhost:5000/carts`)
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Your work has been saved',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
+                })
+        } else {
+            Swal.fire({
+                title: 'Please login to order the food',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login')
+                }
+            })
+        }
+    }
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={image} alt="Shoes" /></figure>
+            <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
+            <div className="card-body flex flex-col items-center">
+                <h2 className="card-title">{name}</h2>
+                <p>{recipe}</p>
+                <div className="card-actions justify-end">
+                    <button onClick={() => handleAddToCart(item)} className="btn btn-outline bg-slate-200 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
---
+++ complete-web-development-course-batch-7/milestone-12/module_77/module_77-8/client/src/pages/Dashboard/MyCart/MyCart.jsx
@@ -0,0 +1,55 @@
+import { Helmet } from "react-helmet-async";
+import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from 'react-icons/fa';
+
+const MyCart = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => item.price + sum, 0)
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | My Cart</title>
+            </Helmet>
+            <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
+                <h3 className="text-3xl">Total Items: {cart.length}</h3>
+                <h3 className="text-3xl">Total Price: ${total}</h3>
+                <button className="btn btn-warning btn-sm">PAY</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Food</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="image" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{item.name}</td>
+                                <td className="text-end">${item.price}</td>
+                                <td>
+                                    <button className="btn btn-ghost btn-lg bg-red-600 text-white"><FaTrashAlt /></button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default MyCart;
